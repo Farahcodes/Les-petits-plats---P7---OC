be20cc11eeaada9bf90498aa264d6f52c7850a0a
farah <farah.fournat@gmail.com>
2023-02-20 13:44:53 +0100

    added dropdownOpen

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,11 @@
 document.querySelector("body").ondragstart = () => {
   return false;
 }; // Prevents images from being dragged on the web page.
+
+function dropdownOpen(dropdown) {
+  if (!dropdown.classList.contains("dropdownOpen")) {
+    dropdownClose();
+    dropdown.classList.add("dropdownOpen");
+    dropdownResize();
+  }
+} // Opens a given dropdown menu by adding the class "dropdownOpen" to it and calling dropdownResize() to adjust its size.
