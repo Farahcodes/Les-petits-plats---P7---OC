d1d0ad6e30d8511d5ec647d6ae6000587acccbdd
farah <farah.fournat@gmail.com>
2023-02-20 13:46:39 +0100

    added dropdownClose function

--- js/dropdown.js
+++ js/dropdown.js
@@ -19,3 +19,12 @@ function dropdownToggle(dropdown) {
     dropdownClose();
   }
 } //  Toggles the open/closed state of a given dropdown menu. If the menu is closed, it opens it by calling dropdownOpen(). If it is open, it closes it by calling dropdownClose()
+
+function dropdownClose() {
+  const dropdownCurrentlyOpen = document.querySelectorAll(".dropdownOpen");
+
+  for (let i = 0; i < dropdownCurrentlyOpen.length; i++) {
+    dropdownCurrentlyOpen[i].classList.remove("dropdownOpen");
+    dropdownCurrentlyOpen[i].style.width = "";
+  }
+} //Closes any currently open dropdown menus by removing the "dropdownOpen" class and resetting their width.
