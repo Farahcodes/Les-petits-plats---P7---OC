aca614abf89fce24de00a632cf79a0efcde6281a
farah <farah.fournat@gmail.com>
2023-02-20 09:57:35 +0100

    added createCard factory

--- js/script.js
+++ js/script.js
@@ -31,6 +31,61 @@ function structureItems(recipes) {
   return [[...setOfIngredients], [...setOfUtensils], [...setOfAppliances]];
 }
 
+// createCard factory
+function createCard(recipe) {
+  const card = document.createElement("article");
+  card.classList.add("card");
+
+  const placeholder = document.createElement("div");
+  placeholder.classList.add("card-img-top");
+  card.appendChild(placeholder);
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  const recipeHeader = document.createElement("div");
+  recipeHeader.classList.add("recipeHeader");
+  const recipeTitle = document.createElement("h2");
+  recipeTitle.classList.add("recipeTitle");
+  recipeTitle.textContent = recipe.name;
+  recipeHeader.appendChild(recipeTitle);
+  const recipeTime = document.createElement("span");
+  recipeTime.classList.add("recipeTime");
+  const recipeImg = document.createElement("img");
+  recipeImg.src = "assets/icons/time.svg";
+  recipeImg.alt = " ";
+  recipeTime.appendChild(recipeImg);
+  const recipeMinutes = document.createElement("span");
+  recipeMinutes.classList.add("recipeMinutes");
+  recipeMinutes.textContent = recipe.time + " min";
+  recipeTime.appendChild(recipeMinutes);
+  recipeHeader.appendChild(recipeTitle);
+  cardBody.appendChild(recipeHeader);
+  const recipeIngredients = document.createElement("ul");
+  recipeIngredients.classList.add("recipeIngredients");
+  recipe.ingredients.forEach((ingredient) => {
+    const recipeIngredient = document.createElement("li");
+    const recipeIngredientName = document.createElement("span");
+    recipeIngredientName.classList.add("recipeIngredientName");
+    recipeIngredientName.textContent = ingredient.ingredient + ": ";
+    recipeIngredient.appendChild(recipeIngredientName);
+    const recipeIngredientValue = document.createElement("span");
+    recipeIngredientValue.classList.add("recipeIngredientValue");
+    recipeIngredientValue.textContent = [
+      ingredient.quantity,
+      ingredient.unit,
+    ].join(" ");
+    recipeIngredient.appendChild(recipeIngredientValue);
+    recipeIngredients.appendChild(recipeIngredient);
+  });
+  cardBody.appendChild(recipeIngredients);
+  const recipePreparation = document.createElement("p");
+  recipePreparation.classList.add("recipePreparation");
+  recipePreparation.textContent = recipe.description;
+  cardBody.appendChild(recipePreparation);
+  card.appendChild(cardBody);
+  return card;
+}
+
 async function init() {
   await structureData(recipes); // awaits the completion of the structureData() function, which is an asynchronous function that returns the unique lists of ingredients, utensils, and appliances from the recipes array.
 }
