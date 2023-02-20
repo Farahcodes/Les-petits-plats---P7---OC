bf08e46ab3f641d880d211a3b85ef108c32ee245
farah <farah.fournat@gmail.com>
2023-02-20 13:45:39 +0100

    added dropdownToggle function

--- js/dropdown.js
+++ js/dropdown.js
@@ -9,3 +9,13 @@ function dropdownOpen(dropdown) {
     dropdownResize();
   }
 } // Opens a given dropdown menu by adding the class "dropdownOpen" to it and calling dropdownResize() to adjust its size.
+
+function dropdownToggle(dropdown) {
+  if (!dropdown.classList.contains("dropdownOpen")) {
+    dropdownClose();
+    dropdown.classList.add("dropdownOpen");
+    dropdownResize();
+  } else {
+    dropdownClose();
+  }
+} //  Toggles the open/closed state of a given dropdown menu. If the menu is closed, it opens it by calling dropdownOpen(). If it is open, it closes it by calling dropdownClose()
