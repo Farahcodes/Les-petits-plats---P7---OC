8d670b713a81afad30fdcaf5de619c7834a4c9c8
farah <farah.fournat@gmail.com>
2023-02-20 09:43:40 +0100

    function to structure recipes items

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-// defining three empty arrays named listOfIngredients, listOfUtensils, and listOfAppliances
+// Defining three empty arrays named listOfIngredients, listOfUtensils, and listOfAppliances
 let listOfIngredients = [];
 let listOfUtensils = [];
 let listOfAppliances = [];
+
+// Function to structure the data: assigns the values returned by the structureItems function
+// to the variables  listOfIngredients, listOfUtensils, and listOfAppliances
+async function structureData(recipes) {
+  [listOfIngredients, listOfUtensils, listOfAppliances] = [
+    ...structureItems(recipes), // returns an array of three arrays (containing the unique values for ingredients, utensils, and appliances in the recipe data)
+  ]; // using array destructuring so that the elements of each of the nested arrays are unpacked and assigned to their respective variables
+}
+
+// function to structure the items
+function structureItems(recipes) {
+  // Initializing three empty sets: The Set data structure is a built-in object in JavaScript that allows us to store a collection of unique values.
+  let setOfIngredients = new Set();
+  let setOfUtensils = new Set();
+  let setOfAppliances = new Set();
+
+  recipes.forEach((recipe) => {
+    setOfAppliances.add(recipe.appliance.toLowerCase()); // using toLowerCase() method to ensure that any duplicates are correctly identified and only a single instance of each unique value is stored in the corresponding Set.
+    recipe.ingredients.forEach((ingredient) => {
+      setOfIngredients.add(ingredient.ingredient.toLowerCase());
+    });
+    recipe.ustensils.forEach((utensil) => {
+      setOfUtensils.add(utensil.toLowerCase());
+    });
+  });
+  // returns an array of three arrays (containing the unique values for ingredients, utensils, and appliances in the recipe data)
+  return [[...setOfIngredients], [...setOfUtensils], [...setOfAppliances]];
+}
