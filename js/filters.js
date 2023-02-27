5c32e1a6ad58e60d6f89afb62cd5f8184d9e3a45
farah <farah.fournat@gmail.com>
2023-02-27 08:59:24 +0100

    creating empty sets

---
+++ js/filters.js
@@ -0,0 +1,4 @@
+// creating empty sets to store the selected ingredients, utensils, and appliances
+let listOfIngredientsSelected = new Set();
+let listOfUtensilsSelected = new Set();
+let listOfAppliancesSelected = new Set();
