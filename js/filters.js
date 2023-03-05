ae3f40ceb970da49986b0f8d32d1f67972d5d1e2
farah <farah.fournat@gmail.com>
2023-03-05 16:28:43 +0100

    adding comments

--- js/filters.js
+++ js/filters.js
@@ -14,12 +14,10 @@ let listOfUtensilsFilteredInput = [];
 let listOfAppliancesFilteredInput = [];
 
 // Creating variables initially containing the full list of recipes
-// recipesTagFiltered:used to store the list of recipes that have been filtered by the tag filters
-// recipesInputFiltered: used to store the list of recipes that have been filtered by the search input
-// recipesFiltered: store the intersection of the two filtered lists
-let recipesTagFiltered = recipes;
-let recipesInputFiltered = recipesTagFiltered;
-let recipesFiltered = recipesInputFiltered;
+
+let recipesTagFiltered = recipes; //used to store the list of recipes that have been filtered by the tag filters
+let recipesInputFiltered = recipesTagFiltered; //used to store the list of recipes that have been filtered by the search input
+let recipesFiltered = recipesInputFiltered; // used to store the intersection of the two filtered lists
 
 // Function that filters the recipes based on selected tags (ingredients, utensils, appliances) and returns the filtered list of recipes.
 function recipesTagFilter() {
