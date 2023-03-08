e6ecf84987ea381098045361b7d354aad0228922
farah <farah.fournat@gmail.com>
2023-03-08 10:00:39 +0100

    recipesTagFilter() function

--- js/filters.js
+++ js/filters.js
@@ -18,3 +18,47 @@ let listOfAppliancesFilteredInput = [];
 let recipesTagFiltered = recipes; //used to store the list of recipes that have been filtered by the tag filters
 let recipesInputFiltered = recipesTagFiltered; //used to store the list of recipes that have been filtered by the search input
 let recipesFiltered = recipesInputFiltered; // used to store the intersection of the two filtered lists (by tags and search input)
+
+//Function that filters the recipes based on selected tags (ingredients, utensils, appliances) and returns the filtered list of recipes.
+function recipesTagFilter() {
+  // Filters for the cards
+  let itemsFiltered = []; // all elements that pass the test function are added to this array
+
+  itemsFiltered = recipesTagFiltered.filter(
+    //checks if each recipe object in recipesTagFiltered matches the criteria specified by the filter conditions.
+    (items) =>
+      // checks if every selected ingredient matches at least one ingredient in the recipe
+      [...listOfIngredientsSelected].every((ingredientSelected) =>
+        items.ingredients.some(
+          (item) => item.ingredient.toLowerCase() === ingredientSelected
+        )
+      ) &&
+      // checks if every selected utensil matches at least one utensil in the recipe
+      [...listOfUtensilsSelected].every((ustensilSelected) =>
+        items.ustensils.some((item) => item.toLowerCase() === ustensilSelected)
+      ) &&
+      // checks if every selected appliance matches the appliance in the recipe
+      [...listOfAppliancesSelected].every(
+        (applianceSelected) =>
+          items.appliance.toLowerCase() === applianceSelected
+      )
+  );
+
+  // Filters for dropdowns
+  [
+    listOfIngredientsFilteredTag,
+    listOfUtensilsFilteredTag,
+    listOfAppliancesFilteredTag,
+  ] = [...structureItems(itemsFiltered)];
+  [
+    listOfIngredientsFiltered,
+    listOfUtensilsFiltered,
+    listOfAppliancesFiltered,
+  ] = [
+    listOfIngredientsFilteredTag,
+    listOfUtensilsFilteredTag,
+    listOfAppliancesFilteredTag,
+  ];
+
+  return itemsFiltered;
+}
