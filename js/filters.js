0cc0a64ddc7c8bc6e57ae4f2a9e3bb1bc9bf6ea9
farah <farah.fournat@gmail.com>
2023-03-08 09:00:24 +0100

    creating empty arrays for tag and input filters

--- js/filters.js
+++ js/filters.js
@@ -2,3 +2,13 @@
 let listOfIngredientsSelected = new Set();
 let listOfUtensilsSelected = new Set();
 let listOfAppliancesSelected = new Set();
+
+// creating empty arrays to store the filtered lists of ingredients, utensils, and appliances for the tag filters
+let listOfIngredientsFilteredTag = [];
+let listOfUtensilsFilteredTag = [];
+let listOfAppliancesFilteredTag = [];
+
+// creating empty arrays to store the filtered lists of ingredients, utensils, and appliances for the input filters
+let listOfIngredientsFilteredInput = [];
+let listOfUtensilsFilteredInput = [];
+let listOfAppliancesFilteredInput = [];
